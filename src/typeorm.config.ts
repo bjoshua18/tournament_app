2176e7b82dfb78237a3eea144fb917fb6d2a4ca9
Byron Bustamante <byrondevweb@gmail.com>
2024-04-15 14:51:20 +0100

    Change datasource config

--- src/typeorm.config.ts
+++ src/typeorm.config.ts
@@ -3,12 +3,8 @@ import {Player} from "@/entity/Player";
 import {Team} from "@/entity/Team";
 
 const config: DataSourceOptions = {
-  type: process.env.DB_TYPE as 'mysql',
-  host: process.env.DB_HOST,
-  port: parseInt(process.env.DB_PORT as string),
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  type: 'postgres',
+  url: process.env.POSTGRES_URL,
   logging: true,
   entities: [Player, Team],
   synchronize: true
